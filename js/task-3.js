901fc4f016a97e55efbc326238a53afff0b7e9e6
vladostapovich <jorabumer@ukr.net>
2024-03-17 17:11:55 +0200

    up

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outPut = document.querySelector("#name-output");
-inputEl.addEventListener("input", (e) => {
-  outPut.innerHTML = e.currentTarget.value;
-  if (inputEl.value.trim().length === 0) {
-    outPut.textContent = "Anonymous";
-  }
-});
